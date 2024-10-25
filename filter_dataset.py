97ab215756fe0350c10e615529ae278d973ee554
Pedro Dantas <pedrood1232@gmail.com>
2024-10-25 16:29:10 -0300

    Update dataset

---
+++ filter_dataset.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv("creditability.csv")
+df_novo = df.drop(
+    [
+        "Duration_of_Credit_monthly",
+        "Instalment_per_cent",
+        "Duration_in_Current_address",
+        "Most_valuable_available_asset",
+        "Age_years",
+        "Type_of_apartment",
+        "Occupation",
+        "No_of_dependents",
+        "Telephone",
+        "Foreign_Worker",
+        "Sex_Marital_Status",
+        "Purpose",
+    ],
+    axis=1,
+)
+
+df_novo.to_csv("dataset.csv", index=False)
